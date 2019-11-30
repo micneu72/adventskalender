52838da149e1952657073da082887c48a471acf2
Michael Neumann <mne@micneu.net>
2019-11-30 14:04:56 +0100

    bilder in grau convertieren gefixt. png´s mit transparentem hintergrund machen noch probleme

--- adventskalender.py
+++ adventskalender.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+""".
+
 # Script Name:      adventskalender.py
 # CreationDate:     04.12.2018
-# Last Modified:    28.11.2019 13:54:43
+# Last Modified:    30.11.2019 12:08:34
 # Copyright:        Michael N. (c)2018
 # Purpose:
 #
@@ -12,25 +15,31 @@
 # apt install python3-opencv
 # pip3 install opencv-python
 # pip3 install pillow
-# brew install tesseract
 #
+"""
+
 from PIL import Image
+from bs4 import BeautifulSoup
+import requests
+import socket
+#import pytesseract
+import cv2
 import sys
 import time
 import math
-import socket
-from bs4 import BeautifulSoup
 import re
 import os.path
-import time
-import requests
-import configparser
-import argparse
-import pytesseract
-import cv2
 import os
-import urllib.request
-import urllib
+import time
+
+
+ZEITDATUM = time.strftime("%d.%m.%Y %H:%M:%S")
+KALENDERURL = "https://www.lc-ellerbekrellingen.de/weihnachtskalender-2018"
+#KALENDERURL = "https://heise.de"
+SEL = '1984307661'
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko)'}
+TEST = True
+
 
 def timepost(start, stop):
     zeit = stop - start
@@ -59,7 +68,7 @@ def timepost(start, stop):
         print("%2d Minute(n), %2d Sekunde(n)" % (int_m, int_s))
     else:
         print("%2d Sekunde(n)" % (int_s))
-        
+
 
 def read_args():
     parser = argparse.ArgumentParser()
@@ -68,8 +77,9 @@ def read_args():
     filename = args.filename
     return filename    
 
-def get_html_code2(uri):
-    page = requests.get(uri)
+
+def get_html_code2(uri, HEADERS):
+    page = requests.get(uri, headers=HEADERS)
     bsObj = BeautifulSoup(page.content, 'html.parser')
     return bsObj
 
@@ -94,20 +104,40 @@ def trenner(laenge, trennerzeichen):
     return str(trennzeile)
 
 
-def getURLs(uri):
-    bereich = ""
-    htmlcode = get_html_code2(uri)
-    urls = sort_data(htmlcode, urlRegEx)
-    return urls
+def create_dir(dirname):
+    os.makedirs(dirname, exist_ok=True)
 
-def createDir(dirname):
-    if not os.path.isdir(dirname):
-        os.makedirs(dirname)
-        
 
-ZEITDATUM = time.strftime("%d.%m.%Y %H:%M:%S")
+def convert_image_to_gray(datei):
+    print("Bilddatei: ", datei)
+    try:
+        img = Image.open(datei).convert('L')
+    except:
+        img = Image.open(datei).convert('LA')
+    img.save(datei)
 
-kalenderurl = "https://www.lc-ellerbekrellingen.de/weihnachtskalender-2018"
+def create_image_list():
+    bilder = []
+    urls = htmlcode.find('img')
+    for url in urls:
+        match = re.search('tablet', url['src'])
+        if match:
+            if not url['src'] == "https://cdn.website-editor.net/c52db4ba56694d33b1086a5b364f6c3a/dms3rep/multi/tablet/Screenshot+2018-08-28+10.19.47-165c58c4.png":
+                #print(url['src'])
+                bilder.append(url['src'])
+    return bilder
+
+
+def init_html_content():
+    htmlcontent = htmlbody
+    htmlcontent += "\t<h3 align=center>Letze Änderung: " + ZEITDATUM + "</h3>\n"
+    return htmlcontent
+
+
+def set_file_name(imguri):
+    extension = re.findall(r'\.[a-z][a-z][a-z]$',imguri)
+    return extension[0]
+
 
 htmlbody = """<!doctype html>
 <html lang="de">
@@ -122,67 +152,76 @@ htmlbody = """<!doctype html>
     <title>Adventskalender OCR für emetriq GmbH</title>
   </head>
   <body>""" + "\n"
-htmlfooter = "<h3 align=center>Letze Änderung: " + ZEITDATUM + "</h3>\n"
-htmlfooter += "<h3 align=center>(c) 2019 Michael Neumann | emetriq GmbH | Likedeeler</h3>"
+htmlfooter = "\t<h3 align=center>Letze Änderung: " + ZEITDATUM + "</h3>\n"
+htmlfooter += "\t<h3 align=center>(c) 2019 Michael Neumann | emetriq GmbH | Likedeeler</h3>"
 htmlfooter +=  """ <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   </body>
 </html> """
 
-createDir("bilder")
-createDir("html")
 
-writehtml = open("html/index.html", "w")
+create_dir("bilder")
+create_dir("html")
 
 # zeitmessung start
 start = time.time()
 
 if __name__ == '__main__':
     NAME = 1
-    bilder = []
-    htmlcode = get_html_code2(kalenderurl)
-    try:
-        imgs = htmlcode.img['src']
-        urls = htmlcode.find_all('img')
-        tage = htmlcode.findAll("div", {"class": "dmNewParagraph"})
-        for url in urls:
-            match = re.search( 'tablet', url['src'])
-            if match:
-                if not url['src'] == "https://cdn.website-editor.net/c52db4ba56694d33b1086a5b364f6c3a/dms3rep/multi/tablet/Screenshot+2018-08-28+10.19.47-165c58c4.png":
-                    #print(url['src'])
-                    bilder.append(url['src'])
-                
-        htmlcontent = htmlbody
-        htmlcontent += "<h3 align=center>Letze Änderung: " + ZEITDATUM + "</h3>\n"
-        for web in bilder:
-            datei = "bilder/" + str(NAME) + ".png"
-            url = web
-            print(url)
-            r = requests.get(url, allow_redirects=True)
-            open(datei, 'wb').write(r.content)
-            image = cv2.imread(datei)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    htmlcode = get_html_code2(KALENDERURL, HEADERS)
+    contentbereich = htmlcode.findAll("div", {"id": SEL})
+    if TEST:
+        htmlcontent = init_html_content()
+        imgs = htmlcode.findAll("img")
+        #print(imgs)
+        for imguri in imgs:
+            print(imguri['src'])
+            fileextension = set_file_name(imguri['src'])
+            imagelocalfile = "bilder/" + str(NAME) + fileextension
+            r = requests.get(imguri['src'], allow_redirects=True)
+            with open(imagelocalfile, 'wb') as f:
+                f.write(r.content)
+            convert_image_to_gray(imagelocalfile)
 
-            filename = "{}.png".format(os.getpid())
-            cv2.imwrite(filename, gray)
+            #text = pytesseract.image_to_string(Image.open(filename))
+            #os.remove(filename)
+            #print(text)
+            #text2 = re.findall("\d{4}", text)
+            #print(text2)
+            #htmlcontent += '<img src="' + imguri + '">' + "\n" + "<br />"
+            #htmlcontent += "<h2>" + str(text2) + "</h2>" + "\n" +"<br />"
+            NAME += 1
+    try:
+        htmlcontent = init_html_content()
+        imgs = contentbereich.findAll("img")
+        print(imgs)
+        for imguri in imgs:
+            datei = "bilder/" + str(NAME) + ".png"
+            print(imguri)
+            r = requests.get(imguri, allow_redirects=True)
+            with open(datei, 'wb') as f:
+                f.write(r.content)
+            convert_image_to_gray(datei) 
 
             text = pytesseract.image_to_string(Image.open(filename))
             os.remove(filename)
             #print(text)
             text2 = re.findall("\d{4}", text)
             print(text2)
-            htmlcontent += '<img src="' + web + '">' + "\n" + "<br />"
+            htmlcontent += '<img src="' + imguri + '">' + "\n" + "<br />"
             htmlcontent += "<h2>" + str(text2) + "</h2>" + "\n" +"<br />"
             NAME += 1
+
     except:
-        htmlcontent = htmlbody
-        htmlcontent += "<h3 align=center>Letze Änderung: " + ZEITDATUM + "</h3>\n"
-        htmlcontent += "<h3 align=center>ist noch nicht gestartet</h3>\n"
+        print("keine bilder im bereich")
+        htmlcontent = init_html_content()
+        htmlcontent += "\t<h3 align=center>ist noch nicht gestartet</h3>\n"
 
     htmlcontent += htmlfooter
-    writehtml.write(htmlcontent)
-    writehtml.close()
+    with open("html/index.html", "w") as f:
+        f.write(htmlcontent)
+
     # zeitmessung stop
     stop = time.time()
 
